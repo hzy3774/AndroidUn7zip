c676efc59b8f21efc76bf4b7b5a42a17708a5270
ransj <ranshengju@gmail.com>
2017-05-13 20:51:02 +0800

    extract 7z file from assets directly

---
+++ un7zip/src/main/jni/src/7zAssetFile.h
@@ -0,0 +1,41 @@
+/* 7zFile.h -- File IO
+2009-11-24 : Igor Pavlov : Public domain */
+
+#ifndef __7Z_ASSETS_H
+#define __7Z_ASSETS_H
+
+
+#include <stdio.h>
+#include "Types.h"
+#include <android/asset_manager.h>
+
+EXTERN_C_BEGIN
+
+/* ---------- File ---------- */
+
+typedef struct
+{
+    AAsset *asset;
+    int64_t length;
+} CSzAssetFile;
+
+WRes InAssetFile_Open(struct AAssetManager *mgr, CSzAssetFile *p, const char *name);
+
+WRes AssetFile_Close(CSzAssetFile *p);
+
+/* reads max(*size, remain file's size) bytes */
+WRes AssetFile_Read(CSzAssetFile *p, void *data, size_t *size);
+
+WRes AssetFile_Seek(CSzAssetFile *p, Int64 *pos, ESzSeek origin);
+
+typedef struct
+{
+    ISeekInStream s;
+    CSzAssetFile assetFile;
+} CAssetFileInStream;
+
+void AssetFileInStream_CreateVTable(CAssetFileInStream *p);
+
+EXTERN_C_END
+
+#endif
